8f9cf1b026d8c5fee5aeba10f07af8529d4f1338
jetiny <86287344@qq.com>
2016-06-15 00:34:38 +0800

    init with layout-window

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # csskit
 css library with sass
+
+mixin     函数
+common    通用
+component 组件
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var bourbon = require("bourbon").includePaths,
+    autoprefix = require("gulp-autoprefixer"),
+    connect = require("gulp-connect"),
+    gulp = require("gulp"),
+    gutil = require('gulp-util'),
+    sass = require("gulp-sass");
+
+var paths = {
+    scss: [
+        "./sass/*.scss",
+        "./sass/**/*.scss"
+    ]
+};
+
+function handleSassError(err) {
+    gutil.log(gutil.colors.red('sass error\r\n'), gutil.colors.yellow(err.message));
+    this.emit('end');
+}
+
+gulp.task("sass", function() {
+    return gulp.src(paths.scss)
+        .pipe(sass({
+            includePaths: ["sass"].concat(bourbon)
+        }))
+        .on('error', handleSassError)
+        .pipe(autoprefix("last 2 versions"))
+        .pipe(gulp.dest("./dist"))
+        .pipe(connect.reload())
+        ;
+});
+
+gulp.task("connect", function() {
+    connect.server({
+        root: "dist",
+        port: 8000,
+        livereload: true
+    });
+});
+
+gulp.task("dev", ["sass", "connect"], function() {
+    gulp.watch(paths.scss, ["sass"]);
+});
