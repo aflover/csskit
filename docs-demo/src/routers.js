3728a2cd100d5eab69d7c8ddd41753d274d3fb89
jetiny <86287344@qq.com>
2016-07-03 22:07:38 +0800

    fddsf

--- docs-demo/src/routers.js
+++ docs-demo/src/routers.js
@@ -6,20 +6,8 @@ import IconPhoton from './components/IconPhoton'
 import IconFontAwesome from './components/IconFontAwesome'
 
 export default {
-    '/Table': {
-        component: Table,
-        title:'Table',
-    },
-    '/Button': {
-        component: Button,
-        title:'Button',
-    },
-    '/IconPhoton': {
-        component: IconPhoton,
-        title:'IconPhoton',
-    },
-    '/IconFontAwesome': {
-        component: IconFontAwesome,
-        title:'IconFontAwesome',
-    },
+    '/Table': { component: Table, title:'Table', },
+    '/Button': { component: Button, title:'Button', },
+    '/IconPhoton': { component: IconPhoton, title:'IconPhoton', },
+    '/IconFontAwesome': { component: IconFontAwesome, title:'IconFontAwesome', },
 }
