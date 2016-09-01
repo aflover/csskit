201b07c84c632612268ddbfc573168b88f5bab79
jetiny <86287344@qq.com>
2016-09-01 23:03:35 +0800

    rewww

--- docs-demo/src/components/Group.vue
+++ docs-demo/src/components/Group.vue
@@ -8,7 +8,7 @@
 	<hr>
 
 	<ul :class="['group-col', tableSize]">
-		<span class="group-title">Favorites</span>
+		<span class="group-title">group-col</span>
 		<li class="group-item" >
 			<span class="icon-inline pt-home"></span><span>Home</span>
 		</li>
@@ -30,66 +30,12 @@
 		</li>
 		<span class="group-divider"></span>
 		<li class="group-item">
-			<span class="icon-inline pt-monitor"></span><span>Desktop</span>
-		</li>
-	</ul>
-
-	<ul :class="['group-col', tableSize]">
-		<span class="group-title">Favorites</span>
-		<li class="group-item" >
-			<span>Home</span><span class="icon-inline pt-home float-right"></span>
-		</li>
-		<li class="group-item active">
-			<span>Photo</span><span class="icon-inline pt-picture float-right"></span>
-		</li>
-		<li class="group-item" >
-			<span>Download</span><span class="icon-inline pt-download float-right"></span>
-		</li>
-		<span class="group-title">Others</span>
-		<li class="group-item" >
-			<span>Document</span><span class="icon-inline pt-folder float-right"></span>
-		</li>
-		<li class="group-item disabled" >
-			<span>Application</span><span class="icon-inline pt-window float-right"></span>
-		</li>
-		<li class="group-item" >
-			<span>AirDrop</span><span class="icon-inline pt-signal float-right"></span>
-		</li>
-		<span class="group-divider"></span>
-		<li class="group-item">
-			<span>Desktop</span><span class="icon-inline pt-monitor float-right"></span>
-		</li>
-	</ul>
-
-	<ul :class="['group-col', 'ak-nav-group', tableSize]">
-		<span class="group-title">Favorites</span>
-		<li class="group-item" >
-			<span class="icon-inline pt-home"></span><span>Home</span>
-		</li>
-		<li class="group-item active">
-			<span class="icon-inline pt-picture"></span><span>Photo</span>
-		</li>
-		<li class="group-item" >
-			<span class="icon-inline pt-download"></span><span>Download</span>
-		</li>
-		<span class="group-title">Others</span>
-		<li class="group-item" >
-			<span class="icon-inline pt-folder"></span><span>Document</span>
-		</li>
-		<li class="group-item disabled" >
-			<span class="icon-inline pt-window"></span><span>Application</span>
-		</li>
-		<li class="group-item" >
-			<span class="icon-inline pt-signal"></span><span>AirDrop</span>
-		</li>
-		<span class="group-divider"></span>
-		<li class="group-item" >
 			<span class="icon-inline pt-monitor"></span><span>Desktop</span>
 		</li>
 	</ul>
 
 	<ul :class="['group-col', 'menu-group', tableSize]">
-		<span class="group-title">Favorites</span>
+		<span class="group-title">+ menu-group</span>
 		<li class="group-item" >
 			<span>Home</span><span class="icon-inline pt-home float-right"></span>
 		</li>
@@ -124,14 +70,71 @@
 				<li class="group-item" >
 					<span>选项1</span>
 				</li>
-				<li class="group-item">
+				<li class="group-item has-sub">
 					<span>选项2</span>
+					<ul :class="['group-col', 'menu-group', tableSize]">
+						<li class="group-item" >
+							<span>选项1</span>
+						</li>
+						<li class="group-item">
+							<span>选项2</span>
+						</li>
+					</ul>
 				</li>
 			</ul>
 		</li>
 	</ul>
 
-
+	<ul :class="['group-col', 'nav-group', tableSize]">
+		<span class="group-title">+ nav-group</span>
+		<li class="group-item" >
+			<span class="icon-inline pt-home"></span><span>Home</span>
+		</li>
+		<li class="group-item active">
+			<span class="icon-inline pt-picture"></span><span>Photo</span>
+		</li>
+		<li class="group-item" >
+			<span class="icon-inline pt-download"></span><span>Download</span>
+		</li>
+		<span class="group-title">Others</span>
+		<li class="group-item" >
+			<span class="icon-inline pt-folder"></span><span>Document</span>
+		</li>
+		<li class="group-item disabled has-sub" >
+			<span>Application</span><span class="icon-inline pt-window float-right"></span>
+			<ul :class="['group-col', 'menu-group', tableSize]">
+				<li class="group-item" >
+					<span>选项1</span>
+				</li>
+				<li class="group-item">
+					<span>选项2</span>
+				</li>
+			</ul>
+		</li>
+		<li class="group-item" >
+			<span>AirDrop</span><span class="icon-inline pt-signal float-right"></span>
+		</li>
+		<span class="group-divider"></span>
+		<li class="group-item has-sub">
+			<span>Desktop</span><span class="icon-inline pt-monitor float-right"></span>
+			<ul :class="['group-col', 'menu-group', tableSize]">
+				<li class="group-item" >
+					<span>选项1</span>
+				</li>
+				<li class="group-item has-sub">
+					<span>选项2</span>
+					<ul :class="['group-col', 'menu-group', tableSize]">
+						<li class="group-item" >
+							<span>选项1</span>
+						</li>
+						<li class="group-item">
+							<span>选项2</span>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
 
 	</div>
 </template>
