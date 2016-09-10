b942d8510a90fc91fca26ce3587286f3c5540ea1
jetiny <86287344@qq.com>
2016-09-10 18:30:31 +0800

    ewew

--- docs-demo/src/components/Layer.vue
+++ docs-demo/src/components/Layer.vue
@@ -5,13 +5,28 @@
 			<flex-control v-model="flexAlign"></flex-control>
 			<label><input type="checkbox" v-model="noHeader"></input>no header</label>
 			<label><input type="checkbox" v-model="noFooter"></input>no footer</label>
-			<label><input type="checkbox" v-model="noBorder"></input>no border</label>
 		</div>
 
 		<button @click="backdrop=!backdrop">layer backdrop</button>
-		<div :class="['layer layer-backdrop', backdrop && 'open']">
-			<div class="dropmask"  @click="backdrop=false"></div>
-			<div :class="['panel', noBorder && 'no-border']" style=" display: inline-block;   min-width: 30%;">
+		<br>
+		<br>
+		<div :class="['panel']" style="width:300px">
+			<button class="btn btn-dismiss">
+				<i class="icon-inline icon-close"></i>
+			</button>
+			<div class="panel-header" v-show="!noHeader">
+				<div class="panel-title">Panel-Title</div>
+			</div>
+			<div class="panel-body">
+				Panel-body
+			</div>
+			<div :class="['panel-footer', flexAlign]" v-show="!noFooter">
+				<button class="btn">取消</button>
+				<button class="btn primary">确定</button>
+			</div>
+		</div>
+		<div :class="['layer backdrop', backdrop && 'open']" @click.self="backdrop=false">
+			<div :class="['panel']" style="min-width:300px">
 				<button class="btn btn-dismiss" @click="backdrop=false">
 					<i class="icon-inline icon-close"></i>
 				</button>
@@ -22,8 +37,8 @@
 					Panel-body
 				</div>
 				<div :class="['panel-footer', flexAlign]" v-show="!noFooter">
-					<button class="btn">取消</button>
-					<button class="btn primary">确定</button>
+					<button class="btn" @click="backdrop=false">取消</button>
+					<button class="btn primary" @click="backdrop=false">确定</button>
 				</div>
 			</div>
 		</div>
@@ -43,7 +58,6 @@
 				flexAlign: '',
 				noHeader: false,
 				noFooter: false,
-				noBorder: false,
 			}
 		},
 		components: {
